b0103c4999a085f90ed9d522c176228a5a03497d
Greg Ryabtsev <149134825+charcoalpro@users.noreply.github.com>
2025-07-05 11:24:09 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Shisha charcoal factory";
+export const SITE_DESCRIPTION = "Manufacturing coconut shell charcoal for shisha in Indonesia";
